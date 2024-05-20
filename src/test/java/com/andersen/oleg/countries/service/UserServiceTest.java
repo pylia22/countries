1ecf508b7399052ca4d76710fce5de2b55710b52
Oleg Pyliavskyi <opy@ethinking.de>
2024-05-20 22:31:11 +0300

    minor refactoring

--- src/test/java/com/andersen/oleg/countries/service/UserServiceTest.java
+++ src/test/java/com/andersen/oleg/countries/service/UserServiceTest.java
@@ -6,25 +6,35 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = DEFINED_PORT)
 class UserServiceTest {
 
     private static final String USER_NAME = "user";
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
     @Mock
     private UserRepository userRepository;
 
     @InjectMocks
-    private UserService userService;
+    private UserService userService = new UserService();
 
     @Test
     void whenUserByNameFoundThenReturnUserDetails() {
